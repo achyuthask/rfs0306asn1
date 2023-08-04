a1ff8279155a9972f79b3783ee145f887b0fbd8e
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-08-04 20:49:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About us</h1>
+      <p>ToyFight is an award-winning creative design agency.
+
+You’ll find the About Page at the top of the menu under the Who section.
+
+This page has a unique feel, thanks to the deconstructed action figures representing the founders, Leigh Whipday and Jonny Lander.
+
+The great attention to detail and interactivity also reflect the company’s 16 years of experience.
+
+To sum up, this page stands out by providing the perfect mix of fun and information.
+Band is a multidisciplinary creative studio based in the Pacific Northwest.
+
+Their About Us page stands out by showcasing some of their unique and creative projects.
+
+No number of words could hope to tell one of their potential clients nearly as much as these pictures can.
+
+In this case, the 25 pictures featured on Band’s About Us page are worth much more than the 170 actual words you’ll read on the page.
+
+The magical visuals and overall simple look and feel make this About Us page one of our top picks.
+This might be the most distinctive website we’ve come across.
+
+Anton & Irene is a design agency based in Manhattan.
+
+Why are they a distinctive web presence?
+
+Because their page takes parallax scrolling to the next level.
+
+The snowy effects, bold colors, and quirky visuals create a truly captivating experience.
+This might be the most distinctive website we’ve come across.
+
+Anton & Irene is a design agency based in Manhattan.
+
+Why are they a distinctive web presence?
+
+Because their page takes parallax scrolling to the next level.
+
+The snowy effects, bold colors, and quirky visuals create a truly captivating experience.
+This might be the most distinctive website we’ve come across.
+
+Anton & Irene is a design agency based in Manhattan.
+
+Why are they a distinctive web presence?
+
+Because their page takes parallax scrolling to the next level.
+
+The snowy effects, bold colors, and quirky visuals create a truly captivating experience.
+</p>
+    </div>
+  );
+}
+
+export default App;
